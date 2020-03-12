f27770479375fcd851ddebf180ce720a094053b4
sifatMoonjerin <moonjerin.sifat@gmail.com>
2020-03-12 19:16:14 +0600

    shop page done

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Cart = props => {
+  const cart = props.cart;
+  const total = cart.reduce((total, product) => total + product.price, 0);
+  let shipping;
+  if(total < 15 && total){
+      shipping = 12.99
+  } else if(total < 35 && total){
+      shipping = 4.99
+  } else{
+      shipping = 0;
+  }
+
+  const tax = total*0.1;
+  const grandTotal = total + shipping + tax;
+
+  const formatNumber = num => Number(num.toFixed(2))
+
+  return (
+    <div>
+      <h4>Order Summary</h4>
+      <p>Items Ordered: {cart.length}</p>
+      <p>Products Price: {formatNumber(total)}</p>
+      <p><small>Tax: {formatNumber(tax)} </small></p>
+      <p><small>Shipping: {formatNumber(shipping)} </small></p>
+      <p>Total: {formatNumber(grandTotal)}</p>
+    </div>
+  );
+};
+
+export default Cart;
